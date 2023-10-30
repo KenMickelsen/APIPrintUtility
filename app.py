96050e1e956ff202a92fef1060ab756cc8558f5f
Ken Mickelsen <kennethmickelsen@gmail.com>
2023-10-30 10:42:03 -0400

    Added functional print job status table

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify, logging
 from requests_toolbelt.multipart.encoder import MultipartEncoder
 from logging.config import dictConfig
+from datetime import datetime
 import uuid
 import socket
 import requests
@@ -23,6 +24,8 @@ dictConfig({
     }
 })
 
+print_jobs = []  # List to hold print jobs data
+
 @app.route('/')
 def index():
     return render_template('index.html') # Your HTML form for submitting print jobs
@@ -37,10 +40,14 @@ def submit_job():
     else:
         return jsonify({"status": "error", "message": "No file provided."}), 400
 
+   # Generate a unique job ID
+    job_id = str(uuid.uuid4())
+
     # Extract data from the form
     data = {
         "queue": request.form.get('queue'),
-        "jobID": str(uuid.uuid4()), # Generate new UUID for job
+        "filename": filename,
+        "jobID": job_id,
         "deviceName": socket.gethostname(),
         "duplex": request.form.get('duplex') == 'true',
         "color": request.form.get('color') == 'true',
@@ -55,6 +62,16 @@ def submit_job():
     files = {
         'file': (filename, file_content)
     }
+
+    # Store initial job details
+    new_job = {
+        "jobID": job_id,
+        "filename": filename,
+        "queue": request.form.get('queue'),
+        "status": "submitted",  # Initial status
+        "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    }
+    print_jobs.append(new_job)
 
     # Send POST request to the Vasion API
     response = requests.post(api_url, data=data, files=files, verify=False)
@@ -72,7 +89,32 @@ def submit_job():
     else:
         return jsonify({"error": "Unexpected response: " + response.text}), response.status_code
 
+@app.route('/print-job-status', methods=['POST'])
+def update_job_status():
+    global print_jobs
+    
+    # Extract the job status details from the request
+    job_details = request.json
+    jobID = job_details.get("jobID")
+    status = job_details.get("status")
+    # Extract other job details from the request (if needed)
+    
+    for job in print_jobs:
+        if job["jobID"] == jobID:
+            job["status"] = status  # Update only the status
+            break
+    
+    # Ensure the print_jobs list doesn't exceed 10 entries
+    while len(print_jobs) > 10:
+        print_jobs.pop(0)
+    
+    return jsonify({"status": "success", "message": "Print job status updated successfully."})
 
+
+@app.route('/get-print-jobs', methods=['GET'])
+def get_print_jobs():
+    global print_jobs
+    return jsonify(print_jobs)
 
 if __name__ == '__main__':
     app.run(debug=True)
